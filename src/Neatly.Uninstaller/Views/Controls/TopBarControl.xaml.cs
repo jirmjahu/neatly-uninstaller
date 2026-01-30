78b922f2b8674e799d7d0e9dd97d6c43013b10af
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-01-30 15:00:33 +0100

    Add top bar panel

---
+++ src/Neatly.Uninstaller/Views/Controls/TopBarControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Neatly.Uninstaller.Views.Controls;
+
+public partial class TopBarControl : UserControl
+{
+    public TopBarControl()
+    {
+        InitializeComponent();
+    }
+}
