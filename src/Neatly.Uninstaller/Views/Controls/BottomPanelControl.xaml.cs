e8a21254bcb4344ba7ea7b36f717c3f7342c4419
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-02-08 17:24:27 +0100

    Show actual application count in bottom panel

--- src/Neatly.Uninstaller/Views/Controls/BottomPanelControl.xaml.cs
+++ src/Neatly.Uninstaller/Views/Controls/BottomPanelControl.xaml.cs
@@ -4,8 +4,12 @@ namespace Neatly.Uninstaller.Views.Controls;
 
 public partial class BottomPanelControl : UserControl
 {
+    
+    public int AppCount => Uninstaller.Instance.AppScanner.FindInstalledApps().Count;
+    
     public BottomPanelControl()
     {
         InitializeComponent();
+        DataContext = this;
     }
 }
